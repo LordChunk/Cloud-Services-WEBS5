92fb0dea5c65e1d4439153ac1a7912873edce567
Job <LordChunk@users.noreply.github.com>
2022-02-02 16:01:38 +0100

    Fixed linter issues

--- first-nodejs-app/routes/users.js
+++ first-nodejs-app/routes/users.js
@@ -1,33 +1,34 @@
 const express = require('express');
+
 const router = express.Router();
 const dbo = require('../services/database');
- 
+
 router.route('/').get((req, res) => {
-    dbo.getDb().then(db => {
-      db.collection("users")
-        .find({}).limit(50)
-        .toArray(function (err, result) {
-          if (err) {
-            res.status(400).send("Error fetching users!");
-          } else {
-              res.json(result);
-          }
-        });
-      }
-    );
-  })
-  .post((req, res) => {
-    dbo.getDb().then(db => {
-      db.collection("users")
-      .insertOne(req.body, function (err, result) {
+  dbo.getDb().then((db) => {
+    db.collection('users')
+      .find({}).limit(50)
+      .toArray((err, result) => {
         if (err) {
-          res.status(400).send("Error inserting user!");
+          res.status(400).send('Error fetching users!');
         } else {
-          console.log('id', result.insertedId);
-          res.status(204).send();
+          res.json(result);
         }
       });
+  });
+})
+  .post((req, res) => {
+    dbo.getDb().then((db) => {
+      db.collection('users')
+        .insertOne(req.body, (err, result) => {
+          if (err) {
+            res.status(400).send('Error inserting user!');
+          } else {
+            // eslint-disable-next-line no-console
+            console.log('id', result.insertedId);
+            res.status(204).send();
+          }
+        });
     });
   });
- 
+
 module.exports = router;
