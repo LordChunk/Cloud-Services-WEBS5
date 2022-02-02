0b9e4f3be093573ea74c3cab00972d9f52fac1ed
Job <LordChunk@users.noreply.github.com>
2022-02-02 16:14:52 +0100

    Fixed styling in tests

--- first-nodejs-app/__tests__/routes/users.test.js
+++ first-nodejs-app/__tests__/routes/users.test.js
@@ -1,49 +1,24 @@
-const { ExpectationFailed } = require('http-errors');
-
-const request = require('supertest')
-
-const app = require('../../app')
-
+const request = require('supertest');
+const app = require('../../app');
 const dbo = require('../../services/database');
 
- 
-
 describe('Get Users', () => {
-
   beforeEach(async () => {
-
-    await dbo.getDb().then(db => db.collection('users').deleteMany({}));
-
+    await dbo.getDb().then((db) => db.collection('users').deleteMany({}));
   });
 
- 
-
-  afterAll(async() => {
-
+  afterAll(async () => {
     dbo.client.close();
-
   });
-
- 
 
   it('should get all users in empty array', async () => {
+    const expected = { foo: 'bar' };
+    await dbo.getDb().then((db) => db.collection('users').insertOne(expected));
 
-    const expected = { 'foo': 'bar' };
+    const res = await request(app).get('/users');
 
-    await dbo.getDb().then(db => db.collection('users').insertOne(expected));
-
- 
-
-    const res = await request(app).get('/users')
-
-     
-
-    expect(res.statusCode).toEqual(200)
-
+    expect(res.statusCode).toEqual(200);
     expect(res.body.length).toEqual(1);
-
     expect(res.body[0]).toMatchObject(expected);
-
   });
-
 });
