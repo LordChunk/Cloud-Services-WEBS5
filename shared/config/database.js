dceb52cdd9fb94f4b9917ff793dcdd9b2a8d19bd
Job <LordChunk@users.noreply.github.com>
2022-04-06 16:08:53 +0200

    Added comment

--- shared/config/database.js
+++ shared/config/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Export db conn based on env
 module.exports = () => {
+  // This prevents the gateway from crashing as our current ECMA version does not support module lazy loading :))
   if(process.env.MONGO_URL !== undefined) {
     mongoose.connect(process.env.MONGO_URL)
   }
