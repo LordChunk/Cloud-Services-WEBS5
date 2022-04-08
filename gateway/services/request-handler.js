45f16e474b125c7898145f7aea3b8c429049318b
Job <LordChunk@users.noreply.github.com>
2022-04-08 11:18:35 +0200

    Added comments

--- gateway/services/request-handler.js
+++ gateway/services/request-handler.js
@@ -5,14 +5,25 @@ class RequestHandler {
     this.circuitBreaker = circuitBreaker;
   }
 
-  send(method, endpoint) {
+  /**
+   * Function that will send the request to the circuit breaker and handle any errors and return the response
+   * @param {String} method  - post or get
+   * @param {String} path - path to the endpoint exluding the base url
+   * @returns 
+   */
+  send(method, path) {
     return (req, res, next) => {
-      this.circuitBreaker.fire(method, endpoint, req.body)
+      this.circuitBreaker.fire(method, path, req.body)
         .then(response => res.status(response.status).json(response.data))
         .catch(error => next(createError(error.response.status, error.message)));
     }
   }
 
+  /**
+   * Function that will create a new RequestHandler instance
+   * @param {CircuitBreaker} circuitBreaker - CircuitBreaker instance for one microservice
+   * @returns {RequestHandler} - RequestHandler instance
+   */
   static createNewRequestHandler(circuitBreaker) {
     return new RequestHandler(circuitBreaker);
   }
