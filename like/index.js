37f336c6851fc31a3489c8fd5f60714ad22e78f2
Job <LordChunk@users.noreply.github.com>
2022-04-08 17:01:08 +0200

    Did initial setup for like service

---
+++ like/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const passport = require('passport');
+const shared = require('cloud-shared');
+
+// Generic Express setup
+app.use(cors());
+app.use(express.json());
+
+// Passport setup
+passport.use(shared.JwtStrategy.InternalStrategy);
+app.use(passport.initialize());
+
+// Register routes
+app.use('/', passport.authenticate('jwt', {session: false}), require('./routes'));
+
+
+app.listen(port,  () => {
+  console.log('Started service at: ' + new Date().toLocaleString())
+  console.log('Authentication service is up on http://localhost:' + port)
+});
+
+module.exports = app;
---
+++ like/routes.js
@@ -0,0 +1,14 @@
+// Startup db connection
+const mongoose = require('mongoose');
+const shared = require('cloud-shared');
+shared.Database(mongoose);
+
+// Import express
+const express = require('express');
+const router = new express.Router();
+
+// Import route specific dependencies
+
+// Routes
+
+module.exports = router;
