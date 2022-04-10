889000a3148493c2dcc7f13d2d4797b9abf165ee
Job <LordChunk@users.noreply.github.com>
2022-04-10 23:30:42 +0200

    Fixed unit tests

--- like/__tests__/like-creation-validation.test.js
+++ like/__tests__/like-creation-validation.test.js
@@ -1,5 +1,10 @@
+process.env.JWT_SECRET = 'secret'
+process.env.API_KEY = 'secret_api_key'
+
+const app = require('../index')
+const mongoose = require('mongoose')
+const request = require('supertest')
 const Like = require('../models/like');
-const mongoose = require('mongoose');
 const Target = require('../models/target');
 
 const like = new Like({
@@ -8,13 +13,27 @@ const like = new Like({
 });
 
 const target = new Target({
-  id: "62531a587e9bf207f5fcdf8c",
-});
+  id: "62531a587e9bf207f5fcdf8c"
+})
+
 
 describe('Like model validation', () => {
+  beforeEach(async () => {
+    await Target.deleteMany({});
+    await Like.deleteMany({});
+  })
+
   it('Cannot create likes for non-existing Targets', async () => {
 
-    const error = await like.validateSync();
-    expect(error.errors['target_id']).toBeDefined();
+    // Expect an error to be thrown when trying to save a like with a non-existing target
+    await expect(like.save()).rejects.toThrow();
+  });
+
+  it('Can create likes for existing Targets', async () => {
+    // Create a target
+    await target.save();
+
+    // Expect no error to be thrown when trying to save a like with an existing target
+    await expect(like.save()).resolves.not.toThrow();
   });
 });
