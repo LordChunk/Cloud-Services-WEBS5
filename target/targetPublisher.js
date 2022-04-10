eab16a87900d2c4ecda987f9d92ac69fbd10932a
Job <LordChunk@users.noreply.github.com>
2022-04-10 14:17:21 +0200

    Fixed rabbitmq authentication

--- target/targetPublisher.js
+++ target/targetPublisher.js
@@ -1,31 +1,30 @@
 var amqp = require('amqplib/callback_api');
 
 function createdTarget(uid) {
-  amqp.connect(process.env.RABBIT_ENDPOINT, function(error0, connection) {
-  if (error0) {
-    console.log(error0);
-    throw error0;
-  }
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-      console.log(error1);
-      throw error1;
+  amqp.connect(process.env.RABBIT_ENDPOINT, function (error0, connection) {
+    if (error0) {
+      console.log(error0);
+      throw error0;
     }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        console.log(error1);
+        throw error1;
+      }
 
-    var exchange = 'target';
+      var exchange = 'target';
 
-    channel.assertExchange(exchange, 'fanout', {
-      durable: true
+      channel.assertExchange(exchange, 'fanout', {
+        durable: true
+      });
+      channel.publish(exchange, '', Buffer.from(uid));
+      console.log(" [x] Sent %s", uid);
     });
-    channel.publish(exchange, '', Buffer.from(uid));
-    console.log(" [x] Sent %s", uid);
-  });
 
-  setTimeout(function() {
-    connection.close();
-    process.exit(0);
-  }, 500);
-});
+    setTimeout(function () {
+      connection.close();
+    }, 500);
+  });
 }
 
 module.exports = createdTarget;
